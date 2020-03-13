979a96fd3ecc009c674c220af3210401aa68ab33
Tyler Croley <tjcroley@outlook.com>
2020-03-13 22:02:00 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,40 @@
+require 'pry'
+
 class Owner
-  # code goes here
+  attr_accessor :owner, :cats
+  attr_reader :species, :name 
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @owner = owner
+    @species = "human"
+    #binding.pry
+    @@all << self
+  end
+ 
+  def say_species
+     @owner = "I am a #{@species}."
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.size 
+  end
+ 
+  def self.reset_all
+    @@all.clear 
+  end
+  
+  def cats(cats)
+    @cats = @owner.count
+    
+    
+  end
+  
+  
+  
 end
