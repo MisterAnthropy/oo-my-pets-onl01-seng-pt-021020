ba12905f86b3378722895170f68cd07bfbf5a7cd
Tyler Croley <tjcroley@outlook.com>
2020-03-13 22:34:34 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -50,7 +50,12 @@ class Owner
     Dog.new(name, self)
   end
   
+  def walk_dogs
+    self.dogs.each {|dog| dog.mood = "happy"}
+  end
   
+  def feed_cats
+    self.cats.each {|cats| cat.mood = "happy"}
     
 
   
